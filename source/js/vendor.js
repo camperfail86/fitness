618950f5fb5944779317da21af81b2bc7ab8f740
camperfail86 <mega.kochkin@inbox.ru>
2023-05-21 15:35:04 +0500

    много чего сделал

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -2,3 +2,18 @@
 import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
 
+let swiper = new Swiper('.mySwiper', {
+  slidesPerView: 4,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+
